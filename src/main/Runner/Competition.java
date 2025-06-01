37d8dfa2d332dba17392a6b34feb5e76af507bc9
Shachar Ifrach <33417983+shaharyi@users.noreply.github.com>
2025-06-01 14:20:13 +0300

    Create Competition.java

---
+++ src/main/Runner/Competition.java
@@ -0,0 +1,19 @@
+
+public class Competition {
+	private String city;
+	private int place;
+	
+	
+	public Competition(String city, int place) {
+		this.city = city;
+		this.place = place;
+	}
+	public String getCity() {
+		return city;
+	}
+	public int getPlace() {
+		return place;
+	}
+	
+	
+}
